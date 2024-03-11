cfb63a5d493a9c2be64dfa2daa170ff20d592264
windbullet <2536810643@qq.com>
2024-03-11 11:57:15 +0800

    提交文件

--- readme.md
+++ readme.md
@@ -2,4 +2,5 @@
 
 [![npm](https://img.shields.io/npm/v/koishi-plugin-gemini-image?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-gemini-image)
 
- 即开即用的gemini，可识别图片，没有上下文记忆
+即开即用的gemini，没有上下文记忆，但可识别图片
+使用的API：https://api.lolimi.cn/
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,57 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, h } from 'koishi'
 
 export const name = 'gemini-image'
 
-export interface Config {}
+export const usage = '识图功能需要 server.temp 服务  \n官方 server-temp 插件需要公网IP，没有的话可以使用其他提供 server.temp 服务的插件'
 
-export const Config: Schema<Config> = Schema.object({})
+export interface Config {
+  forbiddenWords: string[]
+  prefix: string
+}
 
-export function apply(ctx: Context) {
-  // write your plugin here
+export const Config: Schema<Config> = Schema.object({
+  forbiddenWords: Schema.array(Schema.string())
+    .description("违禁词列表，消息包含违禁词时拒绝响应（大小写不敏感）"),
+  prefix: Schema.string()
+    .description("传递给AI消息时添加的前缀文本")
+    .default(""),
+})
+
+export const inject  = {optional: ["server.temp"]}
+
+export function apply(ctx: Context, config: Config) {
+  ctx.command('gemini <message:text>', '与 gemini 对话')
+    .usage('')
+    .option('image', '-i <image:image> 传递给AI的图片')
+    .action(async ({ session, options }, message, image) => {
+      for (let word of config.forbiddenWords) {
+        if (message.toLowerCase().includes(word.toLowerCase())) {
+          return "消息包含违禁词"
+        }
+      }
+
+      let url = `https://api.lolimi.cn/API/AI/gemini.php?msg=${config.prefix + message}`
+
+      if (options.image) {
+        let temp = ctx.get('server.temp')
+        if (!temp) {
+          ctx.logger.warn('server.temp 服务未加载，无法使用图片识别')
+          return "server.temp 服务未加载，无法使用图片识别"
+        }
+        let entry = await temp.create(options.image.src)
+        url += `&img=${entry.url}`
+        entry?.dispose?.()
+      }
+
+      session.send("思考中......")
+      let res = await ctx.http.get(url)
+
+      if (res.code === 200) {
+        return res.data.output
+      } else {
+        ctx.logger.warn(`API 请求错误: ${res.code}`)
+        return `API 请求错误: ${res.code}`
+      }
+
+    })
 }
